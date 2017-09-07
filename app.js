08df6e3a6500ac1fd4e6e8c7c2625c37730e573e
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-09-06 18:20:55 -0600

    Removed path to public folder

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
