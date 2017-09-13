eb6a26791ce6abfd0a631552e7946870e994dc50
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-09-13 11:40:28 -0600

    Fixed Merge Conflict, GET by product complete

--- api/product.js
+++ api/product.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-
 const queries = require('../db/queries');
-
-function isIdValid(req, res, next) {
-  return !isNaN(req.params.id) ? next() : next(new Error("Invalid Id"));
-}
-
-router.get('/', (req, res, next) => {
-  queries.getAllProducts()
-    .then(product => {
-      res.json(product);
-=======
-const queries = require('../db/queries');
-
 const Product = require('../models/Product');
 
 router.get('/', (req, res, next) => {
@@ -23,14 +9,7 @@ router.get('/', (req, res, next) => {
     .eager('[categories, images, seller, product_comments]')
     .then(products => {
       res.json(products);
->>>>>>> ObectionJS
-    }
-  );
+    });
 });
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> ObectionJS
 module.exports = router;
